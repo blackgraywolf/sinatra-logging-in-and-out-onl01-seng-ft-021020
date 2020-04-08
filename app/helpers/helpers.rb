7cfec7dcd42fc3aaddbbd166c410aeb56263b273
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-04-08 17:11:21 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Helpers
   def self.current_user(session_hash)
     @user = User.find(session_hash[:user_id])
@@ -5,5 +6,21 @@ class Helpers
 
   def self.is_logged_in?(session_hash)
     !!session_hash[:user_id]
+=======
+require 'pry'
+class Helpers < ActiveRecord::Base
+
+  def self.current_user(session)
+   
+    @user = User.find_by(session[:session_id])
+    # binding.pry
+  end
+
+  def self.is_logged_in?(session)
+   hi =  !!session[:session_id]
+   yo = session
+    # binding.pry
+     true
+>>>>>>> e35eae79300e086fdbd559a30bb188d139659381
   end
 end
